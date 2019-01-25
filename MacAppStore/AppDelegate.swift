8f735ee336efca47335f4559beb141b90deb5024
Viet Pham <viet@viets-imac.local>
2019-01-25 10:31:27 -0500

    Initial Commit

---
+++ MacAppStore/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacAppStore
+//
+//  Created by Viet Pham on 2019-01-25.
+//  Copyright © 2019 Viet Pham. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+   func applicationDidFinishLaunching(_ aNotification: Notification) {
+      // Insert code here to initialize your application
+   }
+
+   func applicationWillTerminate(_ aNotification: Notification) {
+      // Insert code here to tear down your application
+   }
+
+
+}
+
---
+++ MacAppStore/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacAppStore
+//
+//  Created by Viet Pham on 2019-01-25.
+//  Copyright © 2019 Viet Pham. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+
+      // Do any additional setup after loading the view.
+   }
+
+   override var representedObject: Any? {
+      didSet {
+      // Update the view, if already loaded.
+      }
+   }
+
+
+}
+
